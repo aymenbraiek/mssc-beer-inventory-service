d3a2d164ac0746520d21091d718e8674eaab69f8
aymenbraiek <aymen.braiek@esprit.tn>
2020-04-20 00:53:53 +0100

    Configure Spring Profile for Local MYSQL -closes #5

--- src/main/java/com/biat/msscbeerinventoryservice/repositories/BeerInventoryRepository.java
+++ src/main/java/com/biat/msscbeerinventoryservice/repositories/BeerInventoryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
-@Repository
+
 public interface BeerInventoryRepository extends JpaRepository<BeerInventory, UUID> {
     List<BeerInventory> findAllByBeerId(UUID beerId);
 
